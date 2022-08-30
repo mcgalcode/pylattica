a9e4d5422fc9778e1f916ea98ef42c90419395d3
Max Gallant <mcgallant72@gmail.com>
2022-08-30 15:34:15 -0700

    rm misplaced function

--- rxn_ca/rxn/computing/utils/__init__.py
+++
@@ -1,9 +0,0 @@
-def format_chem_sys(chem_sys: Union[List, str]):
-    if type(chem_sys) is str:
-        arr = chem_sys.split("-")
-        arr.sort()
-        return "-".join(arr)
-    else:
-        copy_arr = copy(chem_sys)
-        copy_arr.sort()
-        return copy_arr
