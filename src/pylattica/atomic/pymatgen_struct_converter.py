fc59058fd5a9fff7b74558989e056ecca3ad6ce8
Max Gallant <mcgallant72@gmail.com>
2023-10-30 16:04:40 -0700

    Add docs

--- src/pylattica/atomic/pymatgen_struct_converter.py
+++ src/pylattica/atomic/pymatgen_struct_converter.py
@@ -4,13 +4,45 @@ from pymatgen.core.lattice import Lattice as PmgLattice
 from ..core import Lattice as PylLattice, StructureBuilder, PeriodicStructure
 
 
-class PymatgenStructureConverter():
-    def to_pylattica_lattice(self, pmg_lat: PmgLattice):
+class PymatgenStructureConverter:
+    """A PymatgenStructureConverter provides utilities for converting pylattica
+    PeriodicStructure and Lattice objects to pymatgen's Structure and Lattice objects.
+    """
+
+    def to_pylattica_lattice(self, pmg_lat: PmgLattice) -> PylLattice:
+        """Converts a pymatgen Lattice to a pylattica Lattice
+
+        Parameters
+        ----------
+        pmg_lat : PmgLattice
+            The pymatgen lattice to convert
+
+        Returns
+        -------
+        Lattice
+            A pylattica lattice
+        """
         pmg_lat = pmg_lat
         pyl_lat = PylLattice(pmg_lat.matrix)
         return pyl_lat
 
-    def to_pylattica_structure_builder(self, pmg_struct: PmgStructure):
+    def to_pylattica_structure_builder(
+        self, pmg_struct: PmgStructure
+    ) -> StructureBuilder:
+        """Converts a pymatgen Structure into a pylattica StructureBuilder which
+        can be used to build pylattica Structures with the same symmetry as the
+        input pymatgen Structure.
+
+        Parameters
+        ----------
+        pmg_struct : PmgStructure
+            The pymatgen structure to convert
+
+        Returns
+        -------
+        StructureBuilder
+            The resulting StructureBuilder
+        """
         lat = self.to_pylattica_lattice(pmg_struct.lattice)
 
         struct_motif = {}
@@ -28,10 +60,34 @@ class PymatgenStructureConverter():
         struct_builder.frac_coords = True
         return struct_builder
 
-    def to_pymatgen_lattice(self, pyl_lat: PylLattice):
+    def to_pymatgen_lattice(self, pyl_lat: PylLattice) -> PmgLattice:
+        """Converts a pylattica Lattice object into a pymatgen Lattice object.
+
+        Parameters
+        ----------
+        pyl_lat : PylLattice
+            The pylattica Lattice to convert
+
+        Returns
+        -------
+        PmgLattice
+            The resulting pymatgen Lattice
+        """
         return PmgLattice(pyl_lat.vecs)
 
-    def to_pymatgen_structure(self, pyl_struct: PeriodicStructure):
+    def to_pymatgen_structure(self, pyl_struct: PeriodicStructure) -> PmgStructure:
+        """Converts a pylattica PeriodicStructure into a pymatgen
+
+        Parameters
+        ----------
+        pyl_struct : PeriodicStructure
+            The pylattica PeriodicStructure to convert
+
+        Returns
+        -------
+        PmgStructure
+            The resulting pymatgen Structure
+        """
         pmg_lat = self.to_pymatgen_lattice(pyl_struct.lattice)
 
         species = []
